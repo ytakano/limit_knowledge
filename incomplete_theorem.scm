041d42370789c31234dfc7be476de4ac75866142
Yuuki Takano <ytakanoster@gmail.com>
2021-10-21 11:21:21 +0900

    不完全性定理を修正

--- incomplete_theorem.scm
+++ incomplete_theorem.scm
@@ -11,13 +11,40 @@
 
 ;; 「xはxである」は証明できない
 (define g
-  '(lambda (x) (L 'is-provable (L x x))))
+  '(lambda (x) (L 'is-unprovable (L x x))))
+
+(print "第一不完全性定理：Gもnot Gも証明できないようなGが存在する")
+
+(print "\ng = " g)
+(print "とする。")
 
 ;; ゲーデル文G
 ;; g(x) = 「xはxである」は証明できない
 ;; G = g(g) = 「「xはxである」は証明できない」に自身を代入した文は証明できない
-(print "(g g)\t\t\t\t\t= " ((eval0 g) g))
+(let (
+      (G ((eval0 g) g))
+      )
+  (print "\n以下のようなGを考える（Gはゲーデル文）。")
+  (print "G = (g g)")
+  (print "  = " G)
+  (print "  ;; 上式の引数を簡約 (L (car G) (eval1 (cadr G)))")
+  (print "  = " (L (car G) (eval1 (cadr G))))
+  (print "  ;; Gと上式の引数が同じ")
+  (print "  ;; (equal? G (eval1 (cadr G))) = " (equal? G (eval1 (cadr G))))
+  (print "  = " (L 'is-unprovable 'G) " ; Gは証明できない")
+  )
 
-(print "(cadr (g g))\t\t\t\t= " (cadr ((eval0 g) g)))
-(print "(eval (cadr (g g)))\t\t\t= " (eval1 (cadr ((eval0 g) g))))
-(print "(eval (cadr (eval (cadr (g g)))))\t= " (eval1 (cadr (eval1 (cadr ((eval0 g) g))))))
+(print "\nGを証明可能と仮定する。")
+(print "すると、(is-unprovable G)が正しいと証明可能となる。")
+(print "つまり、Gは証明できないと証明できることになる。")
+(print "背理法よりGは証明不可能。")
+
+(print "\nnot Gを証明可能と仮定する。")
+(print "ここで、")
+(print "not G = not (is-provable G)")
+(print "      = (provable G)")
+(print "となる。")
+(print "これより、not Gが証明可能となると、Gも証明可能でなければならなので矛盾。")
+(print "背理法よりnot Gが証明不可能。")
+
+(print "\nGもnot Gも証明できない。Q.E.D.")
